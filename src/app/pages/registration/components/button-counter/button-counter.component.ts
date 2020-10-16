6c640694538623541bcc604026bdb05b09510290
Kimia <kimiaa@stud.ntnu.no>
2020-10-16 15:42:16 +0200

    WIP: button counter

---
+++ src/app/pages/registration/components/button-counter/button-counter.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-button-counter',
+  templateUrl: './button-counter.component.html',
+  styleUrls: ['./button-counter.component.scss'],
+})
+export class ButtonCounterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/pages/registration/registration.module.ts
+++ src/app/pages/registration/registration.module.ts
@@ -5,9 +5,10 @@ import { CounterComponent } from './components/counter/counter.component';
 import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { CountOverviewComponent } from './components/count-overview/count-overview.component';
+import { ButtonCounterComponent } from './components/button-counter/button-counter.component';
 
 @NgModule({
-	declarations: [NavigationComponent, CounterComponent, CountOverviewComponent],
+	declarations: [NavigationComponent, CounterComponent, CountOverviewComponent, ButtonCounterComponent],
 	imports: [
 		CommonModule,
 		RouterModule,
@@ -16,7 +17,8 @@ import { CountOverviewComponent } from './components/count-overview/count-overvi
 	exports: [
 		NavigationComponent,
 		CounterComponent,
-		CountOverviewComponent
+		CountOverviewComponent,
+		ButtonCounterComponent
 	]
 })
 export class RegistrationModule { }
