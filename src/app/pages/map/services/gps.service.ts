287f291f1aaaa9d1392bf1dc2fd568d0b936ced1
Kimia <kimiaa@stud.ntnu.no>
2021-01-12 13:55:42 +0100

    Changed from watchPosition to getCurrentPosition

--- src/app/pages/map/services/gps.service.ts
+++ src/app/pages/map/services/gps.service.ts
@@ -11,15 +11,25 @@ export class GpsService {
 
 	constructor(private geolocation: Geolocation) {	}
 
-	 updateTrack(map: L.Map) {
-		this.geolocation.watchPosition({enableHighAccuracy: true}).subscribe(data => {
-			if ('coords' in data) {
-				this.trackedRoute.push({lat: data.coords.latitude, lng: data.coords.longitude});
-				// console.log('Tracked route: ' + JSON.stringify(this.trackedRoute));
-				L.polyline(this.trackedRoute).addTo(map);
-			} else {
-				console.error('There is a Posisiton Error, no coords in data');
-			}
+	//  updateTrack(map: L.Map) {
+	// 	this.geolocation.watchPosition({enableHighAccuracy: false}).subscribe(data => {
+	// 		if ('coords' in data) {
+	// 			this.trackedRoute.push({lat: data.coords.latitude, lng: data.coords.longitude});
+	// 			// console.log('Tracked route: ' + JSON.stringify(this.trackedRoute));
+	// 			L.polyline(this.trackedRoute).addTo(map);
+	// 		} else {
+	// 			console.error('There is a Posisiton Error, no coords in data');
+	// 		}
+	// 	});
+	// }
+
+	updateTrack(map: L.Map) {
+		this.geolocation.getCurrentPosition().then((data) => {
+			this.trackedRoute.push({lat: data.coords.latitude, lng: data.coords.longitude});
+			console.log('Tracked route: ' + JSON.stringify(this.trackedRoute));
+			L.polyline(this.trackedRoute).addTo(map);
+		}).catch((error) => {
+				console.log('Error getting location', error);
 		});
-	 }
+	}
 }
