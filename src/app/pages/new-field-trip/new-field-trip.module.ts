bf9dc987172ccadaf9ddf9b9c73e18d306274989
Kimia <kimiaa@stud.ntnu.no>
2021-01-19 15:00:03 +0100

    added module and small changes

--- src/app/pages/new-field-trip/new-field-trip.module.ts
+++ src/app/pages/new-field-trip/new-field-trip.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
 
 import { NewFieldTripPageRoutingModule } from './new-field-trip-routing.module';
-
 import { NewFieldTripPage } from './new-field-trip.page';
+import { SharedModule } from 'src/app/shared/shared.module';
+
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    NewFieldTripPageRoutingModule
+	SharedModule,
+	CommonModule,
+	FormsModule,
+	IonicModule,
+	NewFieldTripPageRoutingModule
   ],
   declarations: [NewFieldTripPage]
 })
