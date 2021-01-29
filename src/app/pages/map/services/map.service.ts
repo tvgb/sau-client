c1fc45b1d01af0662b8ebc85d6e5f285eecfc35a
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2021-01-29 14:59:01 +0100

    Fixed bug.

--- src/app/pages/map/services/map.service.ts
+++ src/app/pages/map/services/map.service.ts
@@ -91,6 +91,7 @@ export class MapService {
 			deleted: false
 		};
 
+		this.stopDownloads = false;
 		offlineMapMetaData = await this.downloadMapTileArea(offlineMapMetaData);
 
 		if (!offlineMapMetaData.downloadFinished) {
