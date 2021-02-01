ed74099d95f8e045a30b1b6e2af6949c105e189b
Kimia <kimiaa@stud.ntnu.no>
2021-02-01 17:43:40 +0100

    added alertService

---
+++ src/app/shared/services/alert.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class AlertService {
+
+  constructor(private alertController: AlertController) { }
+
+  async confirmAlert(alertHeader: string, alertMessage: string, context: object, confirmFunction) {
+	const alert = await this.alertController.create({
+		cssClass: 'alertConfirm',
+		header: alertHeader,
+		backdropDismiss: true,
+		message: alertMessage,
+		buttons: [
+			{
+				text: 'Nei',
+				role: 'cancel',
+				handler: () => {}
+			}, {
+				text: 'Ja',
+				handler: confirmFunction.bind(context)
+			}
+		]
+	});
+	await alert.present();
+  }
+}
