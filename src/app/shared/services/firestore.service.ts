4d6e6373f54a69ae6a7a820907ca4554f9e3086b
Trym Vegard Gjelseth-Borgen <tvgb@outlook.com>
2021-05-09 15:15:52 +0200

    Fixed writing error.

--- src/app/shared/services/firestore.service.ts
+++ src/app/shared/services/firestore.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { FieldTripInfo } from '../classes/FieldTripInfo';
-import { Beitelag } from '../interfaces/beitelag';
+import { Beitelag } from '../interfaces/Beitelag';
 import { AuthService } from './auth.service';
 
 @Injectable({
